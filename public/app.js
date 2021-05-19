4464c0ca8d703f997ce4d998f1f66251964d342c
Logan Smith <lsmith5@stevens.edu>
2021-05-19 03:18:38 +0000

    edit app.js

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', (_) => {
 	let room = document.getElementById("chat-text");
 	websocket.addEventListener("message", function (e) {
 		let data = JSON.parse(e.data);
-		let chatContent = $(<p><strong>${data.username}</strong>: ${data.text}</p>);
+		let chatContent = '<p><strong>${data.username}</strong>: ${data.text}</p>';
 		room.append(chatContent);
 		room.scrollTop = room.ScrollHeight; //auto scroll to bottom
 	});
